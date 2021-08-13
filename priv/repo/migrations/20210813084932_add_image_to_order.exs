0c5ddd539ec3110fac4320820be8c87cac8c31cd
Linnea Rudenius <linnea.rudenius@epidemicsound.com>
2021-08-13 13:29:03 +0200

    Fix broken migration

--- priv/repo/migrations/20210813084932_add_image_to_order.exs
+++
@@ -1,9 +0,0 @@
-defmodule Birdservice.Repo.Migrations.AddImageToOrder do
-  use Ecto.Migration
-
-  def change do
-    alter table(:orders) do
-      add :image_id, references("images")
-    end
-  end
-end
--- priv/repo/migrations/20210813092143_change_order_image_column.exs
+++ priv/repo/migrations/20210813092143_change_order_image_column.exs
@@ -3,7 +3,6 @@ defmodule Birdservice.Repo.Migrations.ChangeOrderImageColumn do
 
   def change do
     alter table(:orders) do
-      remove :image_id
       add :image_path, :string
     end
   end
